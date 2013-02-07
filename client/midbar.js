081c7a24b36b0f6167ce0aae922845b855066688
Frode T Bjerke <frode.bjerke@gmail.com>
2013-02-07 14:51:53 +0100

    [BOB-3] Showing selected group. Changed create group form. Some styling

--- client/midbar.js
+++ client/midbar.js
@@ -8,13 +8,30 @@ Template.midbar.groups = function() {
 	return Groups.find();
 };
 
-Template.midbar.events({
-	"click .createGroup" : function (event, template) {
+Template.midbar.showAddGroup = function() {
+	return Session.equals("showAddGroup", true);	
+};
 
-		var name = template.find(".groupname").value;
-		console.log('name '+name);
-		Meteor.call("createGroup", {
+Template.midbar.showGroup = function() {
+	var groupId = Session.get("showGroup");
+	return Groups.findOne(Session.get("showGroup"));
+}
+
+Template.midbar.events({
+
+	"click #createGroup" : function (event, template) {
+		var name = template.find("#groupName").value;
+		var success = Meteor.call("createGroup", {
 			groupname: name
-		})
+		});
+		Session.set("showAddGroup", false);
+	},
+
+	"click #addGroup": function (event) {
+		Session.set("showAddGroup", true);
+	},
+
+	"click .group": function (event) {
+		Session.set("showGroup", event.srcElement.id);
 	}
 });  
