986074c56d66a1d283427465d8d31f28d7965300
Frode T Bjerke <frode.bjerke@gmail.com>
2013-02-18 00:31:31 +0100

    added a test framework

---
+++ tests/midbarTest.js
@@ -0,0 +1,27 @@
+// Test the client in this example
+Meteor = {
+	'is_client': true,
+	'is_server': false
+};
+
+// Create a dummy 'hello' template object
+Template = {
+	'midbar': {}
+};
+
+describe('Meteor client-side code', function(){
+
+	describe('Template.midbar', function(){
+
+		describe('#notSignedIn()', function(){
+			it('should return true when user not signed in', function(){
+				Meteor.userId = function() {return "42"};
+				chai.assert.equal(false , Template.midbar.notSignedIn());
+
+				Meteor.userId = function() {return null};
+				chai.assert.equal(true, Template.midbar.notSignedIn());
+			});
+		});
+	});
+
+});
