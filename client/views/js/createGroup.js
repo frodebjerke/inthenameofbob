02e434a575bb3bb114f1cf1b11aa98c8f66fb294
Frode T Bjerke <frode.bjerke@gmail.com>
2013-02-24 19:22:39 +0100

    replaced globalMenu with a dropInArea. Create group and explanation resides there

---
+++ client/views/js/createGroup.js
@@ -0,0 +1,9 @@
+Template.globalMenuView.events({
+	"click #createGroup" : function (event, template) {
+		var name = template.find("#groupName").value;
+		var success = Meteor.call("createGroup", {
+			groupname: name
+		});
+		Session.set("showAddGroup", false);
+	}
+});
