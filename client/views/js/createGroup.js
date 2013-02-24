0928240cfd0707b7f13ca6f178445a4ff335a242
Frode T Bjerke <frode.bjerke@gmail.com>
2013-02-24 19:36:57 +0100

    fix creategroup submit

--- client/views/js/createGroup.js
+++ client/views/js/createGroup.js
@@ -1,4 +1,4 @@
-Template.globalMenuView.events({
+Template.createGroup.events({
 	"click #createGroup" : function (event, template) {
 		var name = template.find("#groupName").value;
 		var success = Meteor.call("createGroup", {
