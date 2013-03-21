192b10c993d48387d7073d690197d7787613a9cb
Frode T Bjerke <frode.bjerke@gmail.com>
2013-03-21 20:32:07 +0100

    upload image when adding person.

--- client/views/js/manage.js
+++ client/views/js/manage.js
@@ -16,16 +16,37 @@ Template.manageView.events({
 		Session.set("showAddPerson", false);
 	},
 
-	"click #submitName": function(event, template) {
+	"click #submitPerson": function(event, template) {
 		var newName = template.find("#personName").value;
-		Meteor.call("addPersonName", this._id, {
-			name: newName
-		});
+		var img = template.find(".img-upload").files[0];
+		var reader = new FileReader();
+		var groupId = this._id;
+
+		reader.onload = function (e) {
+			Meteor.call("addPerson", groupId, {
+				name: newName,
+				image: e.target.result
+			});
+		};
+
+		reader.readAsDataURL(img);
 		Session.set("showAddPerson", false);
 	},
 
 	"click .deletePerson": function(event, template) {
 		Meteor.call("deletePersonFromList", Session.get("showGroup"), this.name);
+	},
+
+	"change .person .img-upload" : function(event, template) {
+		var personname = this.name;
+		var img = template.find(".img-upload").files[0];
+		var reader = new FileReader();
+		reader.onload = function (e) {
+			e.target.result;
+			Meteor.call("uploadPersonImage", Session.get("showGroup"), personname, e.target.result);
+		};
+
+		reader.readAsDataURL(img);
 	}
 });
 
